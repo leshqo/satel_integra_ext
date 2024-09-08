4a17a633e28d1fe77bd235173426f758e104b5bf
michaljonik <michal.jonik@gmail.com>
2024-09-08 18:46:00 +0200

    - bump version

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ test_requirements = [
 
 setup(
     name='satel_integra_ext',
-    version='0.4.1',
+    version='0.4.7',
     description="Communication library and basic testing tool for Satel "
                 "Integra alarm system. Communication via tcpip protocol "
                 "published by SATEL. ",
